edb9f7cc23dee37232217b79d7274cae11cc95db
nguyen trung quang <trunhquang@gmail.com>
2016-03-23 19:48:59 +0700

    st2

---
+++ Lab3/LoginViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Lab3
+//
+//  Created by Dam Vu Duy on 3/23/16.
+//  Copyright © 2016 dotRStudio. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var lblEmail: UITextField!
+    @IBOutlet weak var lblPassword: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func onSignUp(sender: AnyObject) {
+        let user = PFUser()
+        user.username = lblEmail.text
+        user.password = lblPassword.text
+        user.email = lblEmail.text
+        // other fields can be set just like with PFObject
+//        user["phone"] = "415-392-0202"
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo["error"] as? NSString
+                self.showMessage(errorString as! String)
+            } else {
+                self.showMessage("SignUp Success!")
+            }
+        }
+    }
+    
+    @IBAction func onLogIn(sender: AnyObject) {
+        PFUser.logInWithUsernameInBackground(lblEmail.text! , password:lblPassword.text!) {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                self.showMessage("LogIn Success!")
+            } else {
+                self.showMessage("Fail!")
+            }
+        }
+    }
+    
+    func showMessage(string :String){
+        let alert =   UIAlertController(title: "Alert", message: string, preferredStyle: UIAlertControllerStyle.Alert)
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (gy:UIAlertAction) -> Void in
+        }
+        alert.addAction(OKAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+
+    }
+}
+
