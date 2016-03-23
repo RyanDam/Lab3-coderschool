d5f43e503f1109247a73eb3915189144f04c40eb
nguyen trung quang <trunhquang@gmail.com>
2016-03-23 20:28:01 +0700

    st4

--- Lab3/ChatViewController.swift
+++ Lab3/ChatViewController.swift
@@ -11,12 +11,22 @@ import Parse
 
 class ChatViewController: UIViewController {
 
+    @IBOutlet weak var tbvChat: UITableView!
     @IBOutlet weak var chatInputField: UITextField!
+    
+    var chatDatas = [PFObject]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tbvChat.estimatedRowHeight = 50;
+        tbvChat.rowHeight = UITableViewAutomaticDimension
+        
+        tbvChat.delegate = self
+        tbvChat.dataSource =self
+        
+        
+        NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: "onTimer", userInfo: nil, repeats: true)
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,8 +36,8 @@ class ChatViewController: UIViewController {
     
     @IBAction func onSend(sender: AnyObject) {
         let messageObject = PFObject(className: "Message_Swift_032016")
-        messageObject["Text"] = chatInputField.text!
-        messageObject["Username"] = PFUser.currentUser()?.username
+        messageObject["text"] = chatInputField.text!
+        messageObject["user"] = PFUser.currentUser()?.username
         messageObject.saveInBackgroundWithBlock { (success: Bool, err: NSError?) in
             if success {
                 print("Send message success")
@@ -38,14 +48,39 @@ class ChatViewController: UIViewController {
         }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func onTimer{
+        var query = PFQuery(className:"Message_Swift_032016")
+//        query.whereKey("text", equalTo:"Sean Plott")
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(objects!.count) scores.")
+                // Do something with the found objects
+                if let objects = objects {
+                    self.chatDatas = objects
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.tbvChat.reloadData()
+                    })
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!) \(error!.userInfo)")
+            }
+        }
     }
-    */
 
 }
+//MARK: - ExtensionTableView
+extension BusinessesViewController: UITableViewDelegate, UITableViewDataSource{
+    //tableViewDatasource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatDatas?.count ?? 0
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    //tableViewDelegate
+}
+
